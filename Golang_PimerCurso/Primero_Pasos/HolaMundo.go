f301db150d6860596c43fd90dafd8f869ba911c7
sergio <sopekof@gmail.com>
2024-06-26 02:35:29 -0300

    Primeros Pasos

--- Golang_PimerCurso/Primero_Pasos/HolaMundo.go
+++ Golang_PimerCurso/Primero_Pasos/HolaMundo.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo....")
+	fmt.Println("Hola Mundo en GO")
+	fmt.Println(" ")
+	fmt.Println(8+10)
+	fmt.Println(15/3)
+	fmt.Println(!true)
+	fmt.Println(false || true)
+	fmt.Println(" ")
+	adios()
 }
 
+// Funcion en GO
+func adios() {
+	fmt.Println("Adios Mundo")
+}
