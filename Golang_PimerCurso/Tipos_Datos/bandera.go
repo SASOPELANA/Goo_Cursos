744eed6aa0cdca212af5e37058db5f503db0ef5e
sergio <sopekof@gmail.com>
2024-06-27 01:59:44 -0300

    Agregando 27/06/24

---
+++ Golang_PimerCurso/Tipos_Datos/bandera.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 20
+
+	fmt.Println("Valor de x:", x,". Valor de y:", y)
+	fmt.Println("x == y ", x == y)
+	fmt.Println("x != y ", x != y)
+	fmt.Println(" x < y ", x < y)
+	fmt.Println(" x > y ", x > y)
+	fmt.Println(" x <= y ", x <= y)
+	fmt.Println(" x >= y ", x >= y)
+}
