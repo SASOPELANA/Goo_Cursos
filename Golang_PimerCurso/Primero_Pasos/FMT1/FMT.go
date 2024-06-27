97ed30b43feb1f3aa6c3be3b0048f2d3bd4e8cc6
sergio <sopekof@gmail.com>
2024-06-26 23:06:40 -0300

    26/06/24

---
+++ Golang_PimerCurso/Primero_Pasos/FMT1/FMT.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	var edad = 35
+	fmt.Println(edad)
+	fmt.Printf("%T\n", edad) // Muestra el tipo de datos
+	fmt.Printf("%b\n", edad) // Muestra el valor binario
+
+	bandera := true
+	fmt.Println("\n", bandera)
+	fmt.Printf("Bool: %t\n", bandera) // Muestra el tipo de dato bool
+
+
+	// Flotantes y complejos
+	var flotante = 12.5
+	var complejo = complex(12.5,15.5)
+	fmt.Println(flotante)
+	fmt.Println(complejo)
+	*/
+
+	// Diferencia entre print, printf, println.
+	var imprimir = 25
+	fmt.Println(imprimir)
+	fmt.Print(imprimir)
+	fmt.Printf("\n%T, %b, %v, %o, %x", imprimir, imprimir, imprimir, imprimir, imprimir)
+
+}
