40bb59979afc17bd63df031301f5174813f2d5ab
sergio <sopekof@gmail.com>
2024-06-28 01:51:19 -0300

    28/06/24

--- Go_Basico/3_Flujos_Control/main.go
+++ Go_Basico/3_Flujos_Control/main.go
@@ -40,9 +40,47 @@ func main() {
 	fmt.Print("\n---------------------------------------------------------------\n")
 	fmt.Println("\nFunciones en Go.")
 	hola()
+	nombre("Sergio")
+	saluda := devuelve("Alejandro")
+	fmt.Println(saluda)
+
+	fmt.Print("\n---------------------------------------------------------------\n")
+	fmt.Print("\n *** Calculadora Basica ***\n")
+	var a, b int
+
+	fmt.Println("\nIngrese un número: ")
+	fmt.Scanln(&a)
+	fmt.Println("Ingrese otro número: ")
+	fmt.Scanln(&b)
+
+	suma, resta, producto, divi := calculadoraBasica(a, b)
+	fmt.Println("La suma:", suma)
+	fmt.Println("La resta:", resta)
+	fmt.Println("Producto:", producto)
+	fmt.Println("La division:", divi)
+
 }
 
 
+// Funcion que solo muestra 
 func hola(){
 	fmt.Println("Hola desde la función hola()")
+}
+
+// Funcion que pide un valor por parametro
+func nombre(name string) {
+	fmt.Println("Hola", name)
+}
+
+// Funcion que devuelve o retorna un valor
+func devuelve(name string) string {
+	return "Hola " + name
+}
+
+func calculadoraBasica (a, b int) (int, int, int, float32) {
+	suma := a + b
+	resta := a - b
+	producto := a * b
+	division := float32(a) / float32(b)
+	return suma, resta, producto, division
 }
