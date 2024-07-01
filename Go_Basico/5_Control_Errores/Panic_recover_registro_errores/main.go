bb918e26a7607898c1d25c402a6348e434c23148
sergio <sopekof@gmail.com>
2024-07-01 03:38:46 -0300

    1/07/24

--- Go_Basico/5_Control_Errores/Panic_recover_registro_errores/main.go
+++ Go_Basico/5_Control_Errores/Panic_recover_registro_errores/main.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log" // Para mendajes de registros
+	"os"
+)
 
 func main() {
-	divide(100, 10)
-	divide(600, 30)
-	divide(300, 0)
-	divide(500, 200)
+	//divide(100, 10)
+	//divide(600, 30)
+	//divide(300, 0)
+	//divide(500, 200)
+
+
+	// Mensajes de registros. con funcion  -->    log()  registra errores
+	log.Print("Este en un medaje de registro.")
+	log.Println("Este es otro mensaje de registro.")
+
+	// Crea un archivo y guarda los errores
+	log.SetPrefix("main():") // muestra en que funcion se genero el registro
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	defer file.Close() // Cierra el archivo
+
+	log.SetOutput(file)
+	log.Print("!Oye, soy un Log")
 
 }
+
+
+
 
 // Uso de funcion panic, para generar un pila de errores, por entrar en panico la aplicación
 func divide(dividendo int, divisor int) {
