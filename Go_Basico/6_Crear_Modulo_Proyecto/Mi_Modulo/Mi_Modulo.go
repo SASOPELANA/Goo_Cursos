343f9997d22951c96a19d528a635b18234e65079
sergio <sopekof@gmail.com>
2024-07-05 07:40:45 -0300

    Mi Modulo

---
+++ Go_Basico/6_Crear_Modulo_Proyecto/Mi_Modulo/Mi_Modulo.go
@@ -0,0 +1,10 @@
+package Mi_Modulo
+
+import "fmt"
+
+// Hello devuelve un saludo para la persona especificada.
+func Hello(name string) string {
+	// Devuelve un saludo que incluye el nombre en un mensaje
+	message := fmt.Sprintf("¡Hola, %v! ¡Bienvenido!", name)
+	return message
+}
---
+++ Go_Basico/6_Crear_Modulo_Proyecto/Mi_Modulo/go.mod
@@ -0,0 +1,3 @@
+module github.com/SASOPELANA/Goo_Cursos/tree/main/Go_Basico/6_Crear_Modulo_Proyecto/Mi_Modulo
+
+go 1.22.4
