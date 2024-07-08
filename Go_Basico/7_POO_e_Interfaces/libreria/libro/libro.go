8d743536f15e90f07cd0dc701efaa132f20edcff
sergio <sopekof@gmail.com>
2024-07-08 00:24:04 -0300

    08/07/24

--- Go_Basico/7_POO_e_Interfaces/libreria/libro/libro.go
+++ Go_Basico/7_POO_e_Interfaces/libreria/libro/libro.go
@@ -2,23 +2,35 @@ package libro
 
 import "fmt"
 
+// Si colocamos las palabras el primer indice de la variable en mayuscula es un emcasulamiento Publico, si es en minuscula es Privado. Esto es en un struct
 type Libro struct {
-	Titulo  string
-	Autor   string
-	Paginas int
+	titulo  string // ES PRIVADO CUANDO INICIA CON MINISCULA
+	autor   string
+	paginas int
 }
 
 // Simular Constructor en GO con puntero (Referencia)
+// Con el constructor se accede a emcasulamiento privado.
 func NuevoLibro(titulo string, autor string, paginas int) *Libro {
 	return &Libro{
-		Titulo: titulo,
-		Autor:   autor,
-        Paginas: paginas,
+		titulo: titulo,
+		autor:   autor,
+        paginas: paginas,
 	}
 
 }
 
+// Metodo para acceder a atrubutos privado en GO. Metodo get
+func (b *Libro) SetTitulo(titulo string){
+	b.titulo = titulo
+}
+
+// Metodo get para obtener. Para atributos privados en GO
+func (b *Libro) GetTitulo()string{
+	return b.titulo
+}
+
 
 func (b *Libro) PrintInfo() {
-	fmt.Printf("Titulo: %s\nAutor: %s\nPaginas: %d\n", b.Titulo, b.Autor, b.Paginas)
+	fmt.Printf("Titulo: %s\nAutor: %s\nPaginas: %d\n", b.titulo, b.autor, b.paginas)
 }
--- Go_Basico/7_POO_e_Interfaces/libreria/main.go
+++ Go_Basico/7_POO_e_Interfaces/libreria/main.go
@@ -6,17 +6,25 @@ import (
 )
 
 func main() {
+	/*
 	var MiLibro = libro.Libro{
 		Titulo: "Programación en GO",
 		Autor: "Google",
 		Paginas: 300,
 	}
+	*/	
 
-	MiLibro.PrintInfo()
+	// MiLibro.PrintInfo()
 
 	// Usando Constructor
 	fmt.Printf("\nUsando Constructor\n")
 	MiLibro1 := libro.NuevoLibro("Mody Dick", "Herman Melville", 300)
 	MiLibro1.PrintInfo()
 
+	// Acceder a lo atributos privados
+	fmt.Printf("\nUsando Metodo Set y Get, para acceder a un atributo privado.\n")
+	MiLibro1.SetTitulo("Moby Dick (Edición especial)")
+	fmt.Println(MiLibro1.GetTitulo())
+
+	MiLibro1.PrintInfo()
 }
